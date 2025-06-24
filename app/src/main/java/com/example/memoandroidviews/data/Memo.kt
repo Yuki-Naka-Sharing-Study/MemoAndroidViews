bb721ee10852b75a62e5531b30c106e6a6979fec
仲優樹 <yu-ki216m@outlook.jp>
2025-06-24 12:32:06 +0900

    Created Memo data class.

--- app/src/main/java/com/example/memoandroidviews/data/Memo.kt
+++ app/src/main/java/com/example/memoandroidviews/data/Memo.kt
@@ -1,2 +1,11 @@
 package com.example.memoandroidviews.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "memos")
+data class Memo(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val content: String,
+    val timestamp: Long = System.currentTimeMillis()
+)
