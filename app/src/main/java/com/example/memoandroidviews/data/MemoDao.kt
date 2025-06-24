67dd4f5132b6dc6d72b03de9e152080a05280d3f
仲優樹 <yu-ki216m@outlook.jp>
2025-06-24 12:32:45 +0900

    Created MemoDao interface.

--- app/src/main/java/com/example/memoandroidviews/data/MemoDao.kt
+++ app/src/main/java/com/example/memoandroidviews/data/MemoDao.kt
@@ -1,2 +1,16 @@
 package com.example.memoandroidviews.data
 
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface MemoDao {
+    @Query("SELECT * FROM memos ORDER BY timestamp DESC")
+    fun getAllMemos(): LiveData<List<Memo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(memo: Memo)
+
+    @Delete
+    suspend fun delete(memo: Memo)
+}
