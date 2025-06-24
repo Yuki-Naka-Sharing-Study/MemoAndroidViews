7c53f3afb7867f0c29998604cdabe1eed209de22
仲優樹 <yu-ki216m@outlook.jp>
2025-06-24 12:34:28 +0900

    Created MemoRepository.

--- app/src/main/java/com/example/memoandroidviews/repository/MemoRepository.kt
+++ app/src/main/java/com/example/memoandroidviews/repository/MemoRepository.kt
@@ -1,2 +1,16 @@
 package com.example.memoandroidviews.repository
 
+import com.example.memoandroidviews.data.Memo
+import com.example.memoandroidviews.data.MemoDao
+
+class MemoRepository(private val memoDao: MemoDao) {
+    val allMemos = memoDao.getAllMemos()
+
+    suspend fun insert(memo: Memo) {
+        memoDao.insert(memo)
+    }
+
+    suspend fun delete(memo: Memo) {
+        memoDao.delete(memo)
+    }
+}
