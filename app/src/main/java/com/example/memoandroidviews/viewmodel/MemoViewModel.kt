77380b9bee4778b261a81d1759606b5bb94ab6d0
仲優樹 <yu-ki216m@outlook.jp>
2025-06-24 12:36:47 +0900

    Created MemoViewModel.

--- app/src/main/java/com/example/memoandroidviews/viewmodel/MemoViewModel.kt
+++ app/src/main/java/com/example/memoandroidviews/viewmodel/MemoViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.memoandroidviews.viewmodel
 
+import androidx.lifecycle.*
+import com.example.memoandroidviews.data.Memo
+import com.example.memoandroidviews.repository.MemoRepository
+import kotlinx.coroutines.launch
+
+class MemoViewModel(private val repository: MemoRepository) : ViewModel() {
+    val allMemos: LiveData<List<Memo>> = repository.allMemos
+
+    fun insert(content: String) {
+        viewModelScope.launch {
+            repository.insert(Memo(content = content))
+        }
+    }
+
+    fun delete(memo: Memo) {
+        viewModelScope.launch {
+            repository.delete(memo)
+        }
+    }
+}
