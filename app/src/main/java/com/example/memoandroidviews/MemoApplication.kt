4a2a21eff801e5219b5e367238ed137a74521a54
仲優樹 <yu-ki216m@outlook.jp>
2025-06-24 12:50:37 +0900

    Created MemoApplication.

--- app/src/main/java/com/example/memoandroidviews/MemoApplication.kt
+++ app/src/main/java/com/example/memoandroidviews/MemoApplication.kt
@@ -1,2 +1,16 @@
 package com.example.memoandroidviews
 
+import android.app.Application
+import com.example.memoandroidviews.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MemoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@MemoApplication)
+            modules(appModule)
+        }
+    }
+}
