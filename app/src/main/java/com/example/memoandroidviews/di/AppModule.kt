2622917cc47a91aa911b65947d84d6b2ba57ecf4
仲優樹 <yu-ki216m@outlook.jp>
2025-06-24 12:39:04 +0900

    Created AppModule.kt

--- app/src/main/java/com/example/memoandroidviews/di/AppModule.kt
+++ app/src/main/java/com/example/memoandroidviews/di/AppModule.kt
@@ -1,2 +1,27 @@
 package com.example.memoandroidviews.di
 
+import android.app.Application
+import androidx.room.Room
+import com.example.memoandroidviews.data.MemoDatabase
+import com.example.memoandroidviews.repository.MemoRepository
+import com.example.memoandroidviews.viewmodel.MemoViewModel
+import org.koin.android.ext.koin.androidApplication
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+
+    single {
+        Room.databaseBuilder(
+            androidApplication(),
+            MemoDatabase::class.java,
+            "memo_database"
+        ).build()
+    }
+
+    single { get<MemoDatabase>().memoDao() }
+
+    single { MemoRepository(get()) }
+
+    viewModel { MemoViewModel(get()) }
+}
